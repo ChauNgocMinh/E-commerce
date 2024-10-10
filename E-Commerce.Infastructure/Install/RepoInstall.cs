0e72f47c66a0cceeeda5e82571ec2208cfc667ea
X570 Tuf <X570 Tuf@DESKTOP-6PBB0L8>
2024-10-10 22:13:35 +0700

    update

---
+++ E-Commerce.Infastructure/Install/RepoInstall.cs
@@ -0,0 +1,26 @@
+﻿using E_Commerce.Domain.Entities.Products;
+using E_Commerce.Domain.Entities.Roles;
+using E_Commerce.Domain.Entities.Users;
+using E_Commerce.Domain.Interfaces;
+using E_Commerce.Infastructure.DbContexts;
+using E_Commerce.Infastructure.Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Infastructure.Install
+{
+    public class RepoInstall : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IGenericRepository<Product>, GenericRepository<Product>>();
+        }
+    }
+}
