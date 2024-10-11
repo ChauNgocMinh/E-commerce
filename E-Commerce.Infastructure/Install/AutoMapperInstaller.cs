5da9f99a5f5b359a5a989a9d0c4dfe7ed574f30e
minh.chau <minh.chau@bmbsoft.com.vn>
2024-10-11 17:04:58 +0700

    update

--- E-Commerce.Infastructure/Install/AutoMapperInstaller.cs
+++ E-Commerce.Infastructure/Install/AutoMapperInstaller.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using E_Commerce.Application.AutoMapping.ToProductMapping;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace E_Commerce.Infastructure.Install
@@ -7,7 +8,7 @@ namespace E_Commerce.Infastructure.Install
     {
         public void InstallServices(IServiceCollection services, IConfiguration configuration)
         {
-            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddAutoMapper(typeof(ToProductMapping));
         }
     }
 }
