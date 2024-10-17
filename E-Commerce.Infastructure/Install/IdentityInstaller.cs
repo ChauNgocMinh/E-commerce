cb5f8ead5cf2b2ff166561dba1cb1ad27bc54e97
minh.chau <minh.chau@bmbsoft.com.vn>
2024-10-17 16:56:31 +0700

    update

--- E-Commerce.Infastructure/Install/IdentityInstaller.cs
+++ E-Commerce.Infastructure/Install/IdentityInstaller.cs
@@ -4,16 +4,21 @@ using Microsoft.Extensions.DependencyInjection;
 using E_Commerce.Infastructure.DbContexts;
 using Microsoft.AspNetCore.Identity;
 using E_Commerce.Domain.Entities.Roles;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace E_Commerce.Infastructure.Install
 {
-	public class IdentityInstaller : IInstaller
-	{
-		public void InstallServices(IServiceCollection services, IConfiguration configuration)
-		{
-			services.AddIdentity<User, Role>()
-				.AddEntityFrameworkStores<ApplicationDbContext>() 
-				.AddDefaultTokenProviders();
-		}
-	}
+    public class IdentityInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddIdentity<User, Role>(options =>
+            {
+                options.User.RequireUniqueEmail = false;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>() 
+            .AddDefaultTokenProviders();
+        }
+    }
+
 }
