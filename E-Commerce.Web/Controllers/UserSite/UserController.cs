4b2f9d8dd5cb6a661c1d553b71a11b3aac7cff23
minh.chau <minh.chau@bmbsoft.com.vn>
2024-10-17 16:56:44 +0700

    update

---
+++ E-Commerce.Web/Controllers/UserSite/UserController.cs
@@ -0,0 +1,84 @@
+﻿using E_Commerce.Domain.Entities.Users;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers.UserSite
+{
+    public class UserController : Controller
+    {
+        private readonly Microsoft.AspNetCore.Identity.UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public UserController(Microsoft.AspNetCore.Identity.UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(email, password, isPersistent: false, lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(string email, string password, string confirmPassword)
+        {
+            if (password != confirmPassword)
+            {
+                ModelState.AddModelError(string.Empty, "Mật khẩu xác nhận không khớp.");
+                return View();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var user = new User { UserName = email, Email = email };
+                var result = await _userManager.CreateAsync(user, password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
